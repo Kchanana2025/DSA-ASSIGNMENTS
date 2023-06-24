f7b46b44130bf349895bb2375a7fc3b702af0edd
Kchanana2025 <kanikachanana2025@gmail.com>
2023-06-24 13:55:36 +0530

    Create menue_driven.cpp

---
+++ Arrays/menue_driven.cpp
@@ -0,0 +1,39 @@
+//) Develop a Menu driven program to demonstrate the following operations of Arrays
+// 1.CREATE
+// 2.DISPLAY
+// 3.INSERT
+// 4.DELETE
+// 5.SEARCH
+// 6.EXIT
+
+#include <iostream>
+using namespace std;
+void create(int arr[])
+{
+    int n;
+    cout << "ENTER THE SIZE OF ARRAY";
+    cin >> n;
+    cout << "ENTER THE ELEMENTS OF ARRAY";
+}
+int main()
+{
+    int arr[30];
+    int a;
+    cout << "1.CREATE";
+    cout << "2.DISPLAY";
+    cout << "3.INSERT";
+    cout << "4.DELETE";
+    cout << "SEARCH";
+    cout << "EXIT";
+    cout << "ENTER YOUR CHOICE";
+    cin >> a;
+    switch (a)
+    case 1:
+        create(arr);
+case 2:
+case 3:
+case 4:
+case 5:
+
+    return 0;
+}
