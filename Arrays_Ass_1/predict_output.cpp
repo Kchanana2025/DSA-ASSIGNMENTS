797a620318ca5373f981fbf619420ede6d078951
Kchanana2025 <kanikachanana2025@gmail.com>
2023-06-25 17:13:59 +0530

    assignment_1

---
+++ Arrays_Ass_1/predict_output.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int arr[5] = {1};
+    for (i = 0; i < 5; i++)
+        printf("%d ", arr[i]); // similar to cout<<arr[i];
+    return 0;
+}
--- Arrays_Ass_1/remove_dup.cpp
+++ Arrays_Ass_1/remove_dup.cpp
@@ -1,3 +1,5 @@
+//  Design the logic to remove the duplicate elements from an Array and after the
+// deletionthearrayshould contain the uniqueelements.
 #include <iostream>
 using namespace std;
 int remove_duplicates(int *arr, int n)
@@ -6,16 +8,19 @@ int remove_duplicates(int *arr, int n)
     {
         for (int j = i + 1; j < n; j++)
         {
-            if (arr[i] == arr[j])
+            if (arr[i] == arr[j]) // if ke aage == lgta hai
             {
-                int temp = j;
                 while (j < n - 1)
                 {
                     arr[j] = arr[j + 1];
                     j++;
                 }
                 n--;
-                j = temp;
+                j = i;
+                // j=i lene ka ye faida hai ki jab while loop se bahar aa jayega to next iteration me j++ hone se pahle j=i kar do qki jab j++ hoga to automatic j=i+1 ho jaayega aur phir ye current iterator dobara ho jayega required for cases like 1 2 2 2 2
+
+                // j=temp ke index ko phle jaisa kr dia taki andar wala loop poora chl ske because 2 se zyada similar elements bhi toh ho skte thee
+                // eg 5 1 5 3 5 toh agar hum j ko dubara reinitialize na krte toh sirf ek duplicacy jati aur 5 1 3 5 final output aa jata pr.hm temp=j krke loop normal chalate(kyunki j =n-2 yani n-2 index pr na pahucha hota) lekin j=i krke hmne ye iterator bhi dobara chala di
             }
         }
     }
